d3573e513739e47c81a2e23f376ec4664d94646e
bayazid_sustami <bayazidsustamy@gmail.com>
2021-05-06 01:42:29 +0800

    do unit test get tvShow list

--- submissions/app/src/main/java/com/dicoding/submission/jetpack/data/tvShows/TvShowsEntity.kt
+++ submissions/app/src/main/java/com/dicoding/submission/jetpack/data/tvShows/TvShowsEntity.kt
@@ -3,7 +3,5 @@ package com.dicoding.submission.jetpack.data.tvShows
 data class TvShowsEntity(
         val id: String,
         val posterPath: String,
-        val overview: String,
-        val title: String,
-        val releaseDate: String
+        val title: String
 )
