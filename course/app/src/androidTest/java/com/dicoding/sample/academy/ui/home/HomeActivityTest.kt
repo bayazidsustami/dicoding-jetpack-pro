ae162595cedf2d2a19e89cde8194575280d29162
bayazid_sustami <bayazidsustamy@gmail.com>
2021-05-09 23:12:05 +0800

    do instrument test HomeActivity

--- course/app/src/androidTest/java/com/dicoding/sample/academy/ui/home/HomeActivityTest.kt
+++ course/app/src/androidTest/java/com/dicoding/sample/academy/ui/home/HomeActivityTest.kt
@@ -12,22 +12,33 @@ import com.dicoding.sample.academy.utils.DataDummy
 import org.junit.Rule
 import org.junit.Test
 
-class HomeActivityTest{
+class HomeActivityTest {
     private val dummyCourse = DataDummy.generateDummyCourses()
 
     @get:Rule
-        var activityRule = ActivityScenarioRule(HomeActivity::class.java)
+    var activityRule = ActivityScenarioRule(HomeActivity::class.java)
 
     @Test
-    fun loadCourse(){
+    fun loadCourse() {
         delayTwoSecond()
         onView(withId(R.id.rv_academy)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourse.size))
+        onView(withId(R.id.rv_academy)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                dummyCourse.size
+            )
+        )
     }
 
     @Test
-    fun loadDetailCourse(){
-        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+    fun loadDetailCourse() {
+        delayTwoSecond()
+        onView(withId(R.id.rv_academy)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        delayTwoSecond()
         onView(withId(R.id.text_title)).check(matches(isDisplayed()))
         onView(withId(R.id.text_title)).check(matches(withText(dummyCourse[0].title)))
         onView(withId(R.id.text_date)).check(matches(isDisplayed()))
@@ -35,18 +46,48 @@ class HomeActivityTest{
     }
 
     @Test
-    fun loadModule(){
-        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+    fun loadModule() {
+        delayTwoSecond()
+        onView(withId(R.id.rv_academy)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        delayTwoSecond()
         onView(withId(R.id.btn_start)).perform(click())
-        onView(withId(R.id.rv_module)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        onView(withId(R.id.rv_module)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        delayTwoSecond()
         onView(withId(R.id.web_view)).check(matches(isDisplayed()))
     }
 
     @Test
-    fun loadBookmark(){
+    fun loadDetailModule() {
+        delayTwoSecond()
+        onView(withId(R.id.rv_academy)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        delayTwoSecond()
+        onView(withId(R.id.btn_start)).perform(click())
+        delayTwoSecond()
+        onView(withId(R.id.rv_module)).perform(RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(0, click()))
+        delayTwoSecond()
+        onView(withId(R.id.web_view)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun loadBookmark() {
         onView(withText("Bookmark")).perform(click())
+        delayTwoSecond()
         onView(withId(R.id.rv_bookmark)).check(matches(isDisplayed()))
-        onView(withId(R.id.rv_bookmark)).perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(dummyCourse.size))
+        onView(withId(R.id.rv_bookmark)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                dummyCourse.size
+            )
+        )
     }
 
     private fun delayTwoSecond() {
@@ -56,3 +97,5 @@ class HomeActivityTest{
             e.printStackTrace()
 
         }
+    }
+}
