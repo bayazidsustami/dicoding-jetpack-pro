b3b5d6f7cbce51c9532719252f6cb2373c6c1d5c
bayazid_sustami <bayazidsustamy@gmail.com>
2021-05-11 15:53:30 +0800

    do unit test on viewModels

--- submissions/app/src/test/java/com/dicoding/submission/jetpack/ui/mainUI/fragments/movie/MovieViewModelTest.kt
+++ submissions/app/src/test/java/com/dicoding/submission/jetpack/ui/mainUI/fragments/movie/MovieViewModelTest.kt
@@ -1,23 +1,90 @@
 package com.dicoding.submission.jetpack.ui.mainUI.fragments.movie
 
-import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNotNull
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.dicoding.submission.jetpack.data.dataSource.FilmRepository
+import com.dicoding.submission.jetpack.data.movie.MoviesEntity
+import com.dicoding.submission.jetpack.utils.DataDummy
+import com.dicoding.submission.jetpack.utils.Result
+import org.junit.Assert.*
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+import org.mockito.junit.MockitoJUnitRunner
 
+@RunWith(MockitoJUnitRunner::class)
 class MovieViewModelTest {
-
     private lateinit var viewModel: MovieViewModel
+
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var repository: FilmRepository
+
+    @Mock
+    private lateinit var observer: Observer<Result<List<MoviesEntity>>>
+
+    private val dummyMovie = DataDummy.generateMovieData()
 
     @Before
     fun setUp() {
-        viewModel = MovieViewModel()
+        viewModel = MovieViewModel(repository)
     }
 
     @Test
-    fun `get list movie test`(){
-        val movies = viewModel.getMovie()
+    fun `loading get data movie`(){
+        val result = MutableLiveData<Result<List<MoviesEntity>>>()
+        result.value = Result.Loading(data = null)
+
+        val loadingState = Result.Loading<List<MoviesEntity>>(data = null)
+
+        `when`(repository.getDiscoverMovie()).thenReturn(result)
+        val movies = viewModel.getMovie().value
+        verify(repository).getDiscoverMovie()
         assertNotNull(movies)
-        assertEquals(10, movies.size)
+        assertEquals(loadingState, movies)
+
+        viewModel.getMovie().observeForever(observer)
+        verify(observer).onChanged(loadingState)
+    }
+
+    @Test
+    fun `error get data movie`(){
+        val result = MutableLiveData<Result<List<MoviesEntity>>>()
+        result.value = Result.Error(data = null, message = "Something Error")
+
+        val errorState = Result.Error<List<MoviesEntity>>(data = null, message = "Something Error")
+        `when`(repository.getDiscoverMovie()).thenReturn(result)
+        val movies = viewModel.getMovie().value
+        verify(repository).getDiscoverMovie()
+        assertNotNull(movies)
+        assertEquals(errorState, movies)
+
+        viewModel.getMovie().observeForever(observer)
+        verify(observer).onChanged(errorState)
+    }
+
+    @Test
+    fun `success get data movie test`(){
+        val result = MutableLiveData<Result<List<MoviesEntity>>>()
+        result.value = Result.Success(data = dummyMovie)
+
+        val successState = Result.Success(data = dummyMovie)
+
+        `when`(repository.getDiscoverMovie()).thenReturn(result)
+        val movies = viewModel.getMovie().value
+        val listData = movies as Result.Success<List<MoviesEntity>>
+        verify(repository).getDiscoverMovie()
+        assertNotNull(movies)
+        assertEquals(10, listData.data.size)
+
+        viewModel.getMovie().observeForever(observer)
+        verify(observer).onChanged(successState)
     }
 }
